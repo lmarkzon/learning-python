55f63ab3e276bb0bfcba8de75cff664a31a3edfd
Lauren Markzon <lmarkzon@gmail.com>
2015-12-08 07:45:34 -0600

    35%

--- learning_python.py
+++ learning_python.py
@@ -318,4 +318,30 @@ def distance_from_zero(arg):
 
 
 
-#
+# Planning a trip costs function...
+def hotel_cost(nights):
+    return 140 * nights
+
+def plane_ride_cost(city):
+    if city == "Charlotte":
+        return 183
+    elif city == "Tampa":
+        return 220
+    elif city == "Pittsburgh":
+        return 222
+    else:
+        return 475
+
+def rental_car_cost(days):
+    rental = 40 * days
+    if days < 7 and days >= 3:
+        return rental - 20
+    elif days >= 7:
+        return rental - 50
+    else:
+        return rental
+
+def trip_cost(city, days, spending_money):
+    return rental_car_cost(days) + hotel_cost(days) + plane_ride_cost(city) + spending_money
+
+print trip_cost("Los Angeles", 5, 600)
