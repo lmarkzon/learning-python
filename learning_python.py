aa91702c98b84482c22034b4cf26d3cf03b6433a
Lauren Markzon <lmarkzon@gmail.com>
2015-12-08 07:46:12 -0600

    35%

--- learning_python.py
+++ learning_python.py
@@ -345,3 +345,8 @@ def trip_cost(city, days, spending_money):
     return rental_car_cost(days) + hotel_cost(days) + plane_ride_cost(city) + spending_money
 
 print trip_cost("Los Angeles", 5, 600)
+# CONSOLE: 1955
+
+
+
+# 
